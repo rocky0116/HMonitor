40f14709d192c5f801f62b148e9ee8282a99eb94
rocky0116 <wzwang.zhong@163.com>
2018-06-27 09:04:42 +0800

    关于我们

--- HMonitor/Classes/View(视图和控制器)/Profile/HMAboutController.swift
+++ HMonitor/Classes/View(视图和控制器)/Profile/HMAboutController.swift
@@ -10,29 +10,55 @@ import UIKit
 
 class HMAboutController: WBBaseViewController {
 
-    lazy var headView = UIView()
-    lazy var img1 = UIImageView(image: UIImage(named: "idea_add"))
-    lazy var img2 = UIImageView(image: UIImage(named: "idea_no"))
-    lazy var img3 = UIImageView(image: UIImage(named: "idea_no"))
+    lazy var lab = UILabel()
+    lazy var img1 = UIImageView(image: UIImage(named: "about1"))
+    lazy var img2 = UIImageView(image: UIImage(named: "about2"))
+    lazy var img3 = UIImageView(image: UIImage(named: "about3"))
    
    
     override func setupUI() {
         super.setupUI()
         title = "关于我们"
+        self.view.backgroundColor = UIColor.cz_color(withRed: 213, green: 233, blue: 247)
         addUI()
     }
     
    
     
     func addUI(){
-        headView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: 360)
+       
+        self.view.addSubview(img1)
         
-        
-        headView.snp.makeConstraints { (make) in
-            make.width.equalTo(screenWidth)
-            make.height.equalTo(screenHeight - 100)
-            make.top.equalTo(navigationBar.snp.bottom).offset(2)
+        img1.snp.makeConstraints { (make) in
+            make.width.equalTo(180)
+            make.height.equalTo(70)
+            make.top.equalTo(navigationBar.snp.bottom).offset(60)
             make.centerX.equalToSuperview()
+        }
+        
+        self.view.addSubview(lab)
+        lab.text = "V3.0"
+        lab.snp.makeConstraints { (make) in
+            make.width.equalTo(50)
+            make.height.equalTo(30)
+            make.top.equalTo(img1.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.view.addSubview(img2)
+        img2.snp.makeConstraints { (make) in
+            make.height.equalTo(40)
+            make.left.equalToSuperview().offset(50)
+            make.right.equalToSuperview().offset(-50)
+            make.top.equalTo(lab.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.view.addSubview(img3)
+        img3.snp.makeConstraints { (make) in
+            make.height.equalTo(160)
+            make.bottom.equalToSuperview().offset(0)
+            make.width.equalTo(screenWidth)
         }
        
     }
